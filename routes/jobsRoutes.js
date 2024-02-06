839057c7f8ecb99b33f1e5b5b0439f313a285437
modupimaphoto <modupimaphoto@gmail.com>
2024-02-06 18:19:25 +0200

    jobs testing route

--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -4,8 +4,26 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     res.status(200).json({
-        "message": "Job routes"
-    })
-})
+        "message": "All Jobs"
+    });
+});
+
+router.post('/', (req, res) => {
+    res.status(201).json({
+        "message": "Create Job"
+    });
+});
+
+router.post('/', (req, res) => {
+    res.status(200).json({
+        "message": "Update Job"
+    });
+});
+
+router.delete('/', (req, res) => {
+    res.status(200).json({
+        "message": "Delete Job"
+    });
+});
 
 export default router
