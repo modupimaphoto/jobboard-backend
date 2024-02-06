3f9c6e32c676095c43f96e5254ae142027b068cf
modupimaphoto <modupimaphoto@gmail.com>
2024-02-06 18:19:13 +0200

    admin testing route

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,21 +2,9 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.status(200).json({
-        "message": "Admin routes"
-    });
-});
-
 router.post('/', (req, res) => {
-    res.status(201).json({
-        "message": "New job created"
-    })
-})
-
-router.put('/', (req, res) => {
     res.status(200).json({
-        "message": "Job Updated"
+        "message": "Admin Login"
     })
 })
 
